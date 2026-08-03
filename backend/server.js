a18f4b7636ba0f4f2a435df65f38d2f7e44209f7
ikigai-bozz <wilsonebuka18@gmail.com>
2026-08-03 18:45:34 -0400

    Implement persistent order storage

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,44 @@
-const express = require("express");
-const cors = require("cors");
+    const express = require("express");
+    const cors = require("cors");
+    const fs = require("fs");
+    const app = express();
 
-const app = express();
+    app.use(cors());
+    app.use(express.json());
 
-app.use(cors());
-app.use(express.json());
+    const PORT = 3000;
 
-const PORT = 3000;
-
-app.get("/", (req, res) => {
+    app.get("/", (req, res) => {
     res.send("🚀 Kyvora Backend is Running!");
-});
+    });
 
-// ===== CREATE ORDER API =====
-app.post("/api/orders", (req, res) => {
+    // ===== CREATE ORDER API =====
+    app.post("/api/orders", (req, res) => {
 
     const order = req.body;
+
+    // Read existing orders
+    const orders = JSON.parse(
+    fs.readFileSync("orders.json", "utf8")
+    );
 
     console.log("New Order Received:");
     console.log(order);
 
     const orderId = `KYV-${Date.now()}`;
+
+    const newOrder = {
+    orderId,
+    ...order,
+    createdAt: new Date().toISOString()
+    };
+    
+    orders.push(newOrder);
+
+    fs.writeFileSync(
+    "orders.json",
+    JSON.stringify(orders, null, 2)
+    );
 
     res.json({
         success: true,
@@ -28,8 +46,8 @@ app.post("/api/orders", (req, res) => {
         orderId: orderId
     });
 
-});
+    });
 
-app.listen(PORT, () => {
+    app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
-});
+    });
